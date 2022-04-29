a4258f75163b874b7f874469085ea5fc12cdd42e
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-04-29 18:32:52 +0100

    Stakable fees contract and tests

--- contracts/StakableFees.sol
+++ contracts/StakableFees.sol
@@ -6,8 +6,10 @@ pragma solidity ^0.8.0;
 contract StakableFees {
 
     // This is the current stake fee when a user stakes to an entity
-    // Inferred as 100 / 5
-    int64 internal _stakeFee = 20;
+    // Inferred as percentages
+    int64 internal _stakeFee = 5;
+
+    int64 internal _penaltyFee = 80;
 
     function setStakeFee(int64 fee_) public {
         _stakeFee = fee_;
@@ -15,5 +17,14 @@ contract StakableFees {
 
     function getPreStakeFee() public view returns (int64) {
         return _stakeFee;
+    }
+
+    function setPenaltyFee(int64 fee_) public {
+        _penaltyFee = fee_;
+    }
+
+    // If there is a request for a penalty, like a stake position is closed before ending.
+    function getPenaltyFee(bool hasPenalty_) public view returns (int64) {
+        return hasPenalty_ ? _penaltyFee : 0;
     }
 }
